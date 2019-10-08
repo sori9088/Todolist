22fc150b7fee634c8f44d207fbdf1033ce05762c
Hansol <sori9088@naver.com>
2019-10-08 17:41:22 +0700

    first commit

--- js/script.js
+++ js/script.js
@@ -3,7 +3,18 @@ let masterTodoList = [];
 let isDone;
 let num =0;
 
+
+todolist=JSON.parse(localStorage.getItem('data'));
+
+if(todolist== null){
+    todolist=[];
+}else {
+    console.log(todolist)
+    renderTodos('kkk');}
+
+
 function addTodoList() {
+
     const text = document.getElementById("input").value;
 
     const item = {
@@ -12,15 +23,26 @@ function addTodoList() {
         isDone: false
     };
 
-  
     todolist.push(item);
     console.log(todolist);
+
+    localStorage.setItem('data', JSON.stringify(todolist));
 
     renderTodos(todolist);
 
 }
 
+function getTasks() {
+    return JSON.parse(localStorage.getItem("tasks"));
+  }
+  
+  function save(tasks) {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+
+
 function renderTodos(status){
+
     let todos;
     if (status == 'done'){
         todos= todolist.filter(todo => todo.isDone)
@@ -29,13 +51,13 @@ function renderTodos(status){
       } else {
         todos = todolist
       }
-//onclick="onToggle(${i})"
-    console.log(todos)
+
+    console.log("this is " ,todos)
     const html = todos.map((item,i) => {
         return `
         <div class = list>
         <li onclick="onToggle(${item.id})" class="${item.isDone? 'done' : 'undone'}">
-        ${i} ${item.text} 
+         ${item.text} 
         </li>
         <a href="#" onclick="remove(${item.id})">X</a>
         </div>
@@ -44,33 +66,16 @@ function renderTodos(status){
     document.getElementById("area").innerHTML = html;
     document.getElementById("input").value = '';
 }
-// function renderTodos(status) {
-//     let todos;
-//         if (status == 'done'){
-//             todos= todolist.filter(todo => todo.isDone)
-//         } else if (status == 'undone') {
-//             todos = todolist.filter(todo => !todo.isDone )
-//           } else {
-//             todos = todolist
-//           }
-
-//     const html = todos.map((item,i) => {
-//                 return `
-//                 <li onclick="onToggle(${i})" 
-//                 class="${item.isDone? 'done' : 'undone'}">
-//                 ${item.text} 
-//                 <a href="#" onclick="remove(${i})">X</a>
-//                 <a href="#" onclick="onToggle(${i})">do</a></li>`
-//             }).join('');
-//      document.getElementById("area").innerHTML = html;
-//      document.getElementById("input").value = '';
-// }
 
 
 function remove(id) {
+
     todolist = todolist.filter(item => {
         return item.id != id;
     })
+
+    localStorage.setItem('data', JSON.stringify(todolist));
+    
     // todolist.splice(index,1);
     renderTodos(todolist);
 }
